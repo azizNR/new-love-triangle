ee10f32f33879ea58784c87ad9019dd04d20cc94
Aziz <aziz.narkhabilov@mail.ru>
2019-07-06 18:46:03 +0500

    add -A

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+      let result = 0;
+
+    preferences.forEach((element, i) => {
+
+        let secondElem = preferences[element - 1]
+        let thirdElem = preferences[secondElem - 1]
+        if (
+            preferences[preferences[element - 1] - 1] === i + 1 &&
+    element !== secondElem
+) {
+        result++
+    }
+})
+    result /= 3;
+    return result;
 };
